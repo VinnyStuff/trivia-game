4f4536e9e2733369b913b800905c503665612725
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2023-02-19 15:21:39 -0300

    start vue project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Trivia from './components/Trivia.vue'
+</script>
+
+<template>
+  <div class="Game">
+    <Trivia/>
+  </div>
+</template>
+
+<style scoped>
+
+.Game{
+  width: 100vw;
+  height: 100vh;
+  background-color: #F6F5F5; 
+
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+}
+
+
+</style>
