64f96a5dc0f6e1cde521220288e834c8190f2c24
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2023-02-25 17:01:54 -0300

    working about show answers in single questions

--- src/components2/TriviaResults.vue
+++ src/components2/TriviaResults.vue
@@ -1,41 +1,41 @@
 <template>
     <div class="trivia-results">
-        <div class="questions" v-for="(questions, questionsIndex) in questionsObject" :key="questionsIndex" :class="{ 'correct-answer': checkAnswer(questionsIndex) === 'correct', 'wrong-answer': checkAnswer(questionsIndex) === 'wrong', 'not-answered': checkAnswer(questionsIndex) === 'notAnswered'}">
+        <div class="questions" v-for="(questions, questionsIndex) in questionsObject" :key="questionsIndex" :class="{ 'correct-answer': checkIfQuestionsIsCorrect(questionsIndex) === 'correct', 'wrong-answer': checkIfQuestionsIsCorrect(questionsIndex) === 'wrong', 'not-answered': checkIfQuestionsIsCorrect(questionsIndex) === 'notAnswered'}">
             <div class="question-state" >
-                <Correct v-if="checkAnswer(questionsIndex) === 'correct'"/>
-                <NotAnswered v-else-if="checkAnswer(questionsIndex) === 'notAnswered'"/>
-                <Wrong v-else-if="checkAnswer(questionsIndex) === 'wrong'"/>
+                <Correct v-if="checkIfQuestionsIsCorrect(questionsIndex) === 'correct'"/>
+                <NotAnswered v-else-if="checkIfQuestionsIsCorrect(questionsIndex) === 'notAnswered'"/>
+                <Wrong v-else-if="checkIfQuestionsIsCorrect(questionsIndex) === 'wrong'"/>
             </div>
             <p>{{ decodeEntities(questionsObject[questionsIndex].question) }}</p>
 
-            <div class="dropdown-answers">
-                <v-menu transition="scale-transition">
-                    <template v-slot:activator="{ props }">
-                    <v-btn color="primary" v-bind="props">
-                    bla
-                    </v-btn>
-                    </template>
-                    <v-list>
-                    <v-list-item v-for="(questionsAnswers, questionsAnswersIndex) in allQuestionsAnswers" :key="questionsAnswersIndex">
-
-                    <v-list-item-title>{{ questionsAnswers[questionsAnswersIndex] }}</v-list-item-title>
-                    </v-list-item>
-                    </v-list>
-                </v-menu>
+            <div class="chevron-down-button" @click="">
+                <ChevronDown/>
             </div>
+            <div class="questions-answers">
+                <div v-for="(currentQuestionsAnswers, answersIndex) in allQuestionsAnswers[questionsIndex]" :key="questionsIndex">
+                    <div>
+                        <p>{{ allQuestionsAnswers[questionsIndex][answersIndex] }}</p>
+                    </div>
+                </div>
+            </div> 
         </div>
     </div>
-
 </template>
   
 <script setup>
 import Wrong from '../components/icons/Wrong.vue'
 import Correct from '../components/icons/Check.vue'
 import NotAnswered from '../components/icons/QuestionMark.vue'
+import ChevronDown from '../components/icons/ChevronDown.vue'
 </script>
 
 <script>
 export default {
+    data(){
+        return{
+            showQuestionsAnswers: false,
+        }
+    },
     props:{
         questionsObject: Object,
         allQuestionsAnswers: Array,
@@ -49,7 +49,7 @@ export default {
             el.innerHTML = text;
             return el.value;
         },
-        checkAnswer(currentQuestionIndex){
+        checkIfQuestionsIsCorrect(currentQuestionIndex){
             if (this.questionsAnswered[currentQuestionIndex] === this.correctAnswers[currentQuestionIndex]){
                 return 'correct'
             }
@@ -59,7 +59,7 @@ export default {
             else{
                 return 'wrong';
             }
-        }
+        },
     } 
 }
 </script>
@@ -148,4 +148,72 @@ export default {
 .not-answered > .question-state > svg{
     fill: #8B8B8B;
 }
+
+.chevron-down-button{
+    width: 30px;
+    height: 30px;
+
+    position: absolute;
+    top: 50%;
+    right: 1.5%;
+    transform: translateY(-50%);
+}
+.chevron-down-button > svg{
+    width: 22px;
+    height: 22px;
+
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    stroke: #3A3A3A;
+    stroke-width: 3px;
+}
+.chevron-down-button:hover{
+    cursor : pointer;
+    opacity: 0.5;
+    transition: 100ms ease;
+}
+.chevron-down-button:active{
+    opacity: 1;
+    transition: 100ms ease;
+}
+.correct-answer > .chevron-down-button > svg{
+    stroke: white;
+    stroke-width: 3px;
+}
+.wrong-answer > .chevron-down-button > svg{
+    stroke: #ED4956;
+    stroke-width: 3px;
+}
+.not-answered > .chevron-down-button > svg{
+    stroke: #8B8B8B;
+    stroke-width: 3px;
+}
+
+.questions-answers{
+   /*  position: absolute; */
+    background-color: white;
+    width: 100%;
+
+    left: 0;
+    margin-top: 9.5%;
+    z-index: 9999;
+
+    -webkit-box-shadow: 0px 9px 18px -4px rgba(0,0,0,0.5); 
+    box-shadow: 0px 9px 18px -4px rgba(0,0,0,0.5);
+    border-radius: 20px;
+}
+.questions-answers > p{
+    font-size: 18px;
+    text-align: justify;
+    margin: 0px 30px;
+    line-height: 22px;
+    color: #3A3A3A;
+}
+.questions-answers > div{
+    border: 2px solid red;
+}
+
 </style>
