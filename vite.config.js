b336104550119a84d169f5550d9230cba5d614a0
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2023-03-12 16:18:22 -0300

    update gh pages

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -5,6 +6,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/trivia-game/'
+    : '/',
   plugins: [vue()],
   resolve: {
     alias: {
