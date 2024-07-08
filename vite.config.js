af3bfc06c87c7b00d4ea713fd2e76d55b6f4b3a2
Vinnycios <vinnyciosdev@gmail.com>
2024-07-08 16:00:05 -0300

    test to vercel

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/trivia-game/',
-
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
